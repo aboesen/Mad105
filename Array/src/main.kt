24e87b7f95eb6d60c510328226aa47f71151eacb
aboesen <55418558+aboesen@users.noreply.github.com>
2021-06-19 16:34:36 -0500

    Add files via upload

---
+++ Array/src/main.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var storeList= arrayOf<String>("Chocolate Ice Cream","Vanilla Ice Cream","Strawberry Ice Cream","Waffle Cones","Sprinkles")
+    for(sitem in storeList){
+        println(sitem)
+    }
+}
